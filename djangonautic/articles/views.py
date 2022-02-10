a189659e3f951ba8d39fb3cdd6d0f68d0bfa287f
njal3001 <njal_telsto@hotmail.com>
2022-02-10 21:31:28 +0100

    Article detail page

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -6,3 +6,9 @@ def article_list(request):
     articles = Article.objects.all().order_by('date')
     return render(request, 'articles/article_list.html',
                   {'articles': articles})
+
+
+def article_detail(request, slug):
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_detail.html',
+                  {'article': article})
